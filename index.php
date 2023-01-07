8d253e365cb1210c56a0b777fb44301fa2e90d9d
RussellShire <russell.shire@gmail.com>
2023-01-07 13:00:18 +0000

    Add loop for creating random number boxes

--- index.php
+++ index.php
@@ -4,7 +4,28 @@
 include "numbers.php";
 include "random_number.php";
 
-echo getRandomNumber();
+$rand_num_str = getRandomNumber();
+$rand_num_char = str_split($rand_num_str);
+
+?>
+<div style="display:flex">
+<?php
+foreach ($rand_num_char as $char) : ?>
+    <div style="
+        border:solid;
+        padding:15px 10px;
+        margin:5px">
+        <!-- <?= $char; ?> -->
+        
+    </div>
+<?php endforeach; 
+?>
+</div>
+<?php
+
+
+echo $rand_num_str . "<br>";
+print_r($rand_num_char);
 ?>
 
 </body>
