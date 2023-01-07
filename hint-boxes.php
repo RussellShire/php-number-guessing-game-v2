4ef4be9bf1a002dcea83d1c79c560ae745bf6a45
RussellShire <russell.shire@gmail.com>
2023-01-07 13:22:05 +0000

    Add form to submit guesses

--- hint-boxes.php
+++ hint-boxes.php
@@ -6,8 +6,10 @@ foreach ($rand_num_char as $key => $char) : ?>
         padding:15px 10px;
         margin:5px">
         <?php
-        if($guess_char[$key] === $char) {
+        if($guess_str && $guess_char[$key] === $char) {
             echo $char;
+        } else {
+            echo "*";
         }
         
         ?>
--- index.php
+++ index.php
@@ -7,9 +7,21 @@ include "random_number.php";
 $rand_num_str = getRandomNumber();
 $rand_num_char = str_split(strtoupper($rand_num_str));
 
-$guess_str = "three";
-$guess_char = str_split(strtoupper($guess_str));
+$guess_str = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["input"])) {
+    $guess_str = trim(htmlspecialchars($_POST["input"]));
+    $guess_char = str_split(strtoupper($guess_str));
+}
+
+?>
+
+<form method="post">
+    <input type="text" name="input" value="<?= $guess_str ?>"/>
+    <button type="submit">Guess</button>
+</form>
+
+<?php
 include "hint-boxes.php";
 
 echo $rand_num_str . "<br>";
